12a4303f58271bb1af8f16a07352f8b37dcb9787
Prajal Gupta <gupta.prajal@gmail.com>
2022-05-21 22:23:04 +0530

    2nd commit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,6 +4,7 @@ import Auxillary from '../../../hoc/Auxillary/Auxillary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+    // This could be a functional component, doesn't have to be a class
     componentWillUpdate() {
         console.log('[OrderSummary] WillUpdate');
     }
